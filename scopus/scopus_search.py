d3cc9fa79679b776d70fd2926cb7daaa09370619
John Kitchin <jkitchin@andrew.cmu.edu>
2016-07-11 10:21:35 -0400

    mostly change triple ''' to """, also change variables to semi-private with _
    That makes it possible to have documented properties.

--- scopus/scopus_search.py
+++ scopus/scopus_search.py
@@ -15,7 +15,13 @@ if not os.path.exists(SCOPUS_SEARCH_DIR):
 
 
 class ScopusSearch(object):
-    '''Class to search a query, and retrieve a list of EIDs as results.'''
+    """Class to search a query, and retrieve a list of EIDs as results."""
+    @property
+    def EIDS(self):
+        """Return list of EIDs retrieved."""
+        return self._EIDS
+
+
     def __init__(self,
                  query,
                  fields='eid',
@@ -23,12 +29,14 @@ class ScopusSearch(object):
                  start=0,
                  refresh=False,
                  max_entries=1000):
-        '''
+        """A Scopus Search query.
+
         query is a string of the query.
         fields is the list of fields you want returned.
 
-        Results are cached in SCOPUS_SEARCH_DIR/{query}.
-        '''
+        XML results are cached in SCOPUS_SEARCH_DIR/{query}.
+        The EIDs are stored as a property.
+        """
 
         self.query = query
 
@@ -39,9 +47,9 @@ class ScopusSearch(object):
 
         if os.path.exists(qfile) and not refresh:
             with open(qfile) as f:
-                self.EIDS = [eid for eid in
-                             f.read().strip().split('\n')
-                             if eid]
+                self._EIDS = [eid for eid in
+                              f.read().strip().split('\n')
+                              if eid]
         else:
             url = 'http://api.elsevier.com/content/search/scopus'
             # No cached file exists, or we are refreshing.
@@ -60,13 +68,13 @@ class ScopusSearch(object):
                 N = int(N.text)
             except:
                 N = 0
-
+                
             if N > max_entries:
                 raise Exception(('N = {}. '
                                  'Set max_entries to a higher number or '
                                  'change your query').format(N))
 
-            self.EIDS = []
+            self._EIDS = []
             while N > 0:
                 resp = requests.get(url,
                                     headers={'Accept': 'application/json',
@@ -79,7 +87,7 @@ class ScopusSearch(object):
                 results = resp.json()
 
                 if 'entry' in results['search-results']:
-                    self.EIDS += [str(r['eid']) for
+                    self._EIDS += [str(r['eid']) for
                                   r in results['search-results']['entry']]
                 start += count
                 N -= count
@@ -89,9 +97,9 @@ class ScopusSearch(object):
                     f.write('{}\n'.format(eid))
 
     def __str__(self):
-        s = '''{self.query}
+        s = """{self.query}
         Resulted in {N} hits.
-    {entries}'''
+    {entries}"""
         return s.format(self=self,
                         N=len(self.EIDS),
                         entries='\n    '.join(self.EIDS))
