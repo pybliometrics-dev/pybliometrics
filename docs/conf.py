0af93246751ffa50203b085019ae8b68936a90f1
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-04-08 01:45:21 +0200

    Use autosummary to document classes

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ needs_sphinx = '1.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
