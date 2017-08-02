74f96b5f96ec0ce05a66db830014a840649f6a97
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-08-02 17:56:38 +0200

    Use pbr for versionizing

--- scopus/__init__.py
+++ scopus/__init__.py
@@ -1,3 +1,9 @@
+from pbr.version import VersionInfo
+
+_v = VersionInfo('scopus').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
+
 from scopus.utils import *
 
 from scopus.scopus_affiliation import *
--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-setup(name='scopus',
-      version='0.2.1',
-      description='Python API for Scopus',
-      url='http://github.com/scopus-api/scopus',
-      author='John Kitchin and Michael E. Rose',
-      author_email='jkitchin@andrew.cmu.edu or Michael.Ernst.Rose@gmail.com',
-      license='MIT',
-      packages=['scopus', 'scopus.utils'],
-      scripts=['scopus/bin/scopus_coauthors'],
-      test_suite = 'nose.collector',
-      long_description='''Provides Python functions to retrieve data from the Scopus APIs.''',
-      install_requires=['nose'])
+setup(setup_requires=['pbr'], pbr=True)
