e4ccce6dd1dcc89996ecf4b44971245c8484bc3d
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-08-23 15:59:55 -0400

    Take API Key loading out of __init__; fix #45 with python version detection

--- scopus/utils/get_content.py
+++ scopus/utils/get_content.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import sys
 
 import scopus
 
@@ -32,15 +33,23 @@ def download(url, params=None, accept="xml"):
     -------
     resp : byte-like object
         The content of the file, which needs to be serialized.
+
+    Notes
+    -----
+    Loads the API Key into the scopus namespace on first run.
     """
     accepted = ("json", "xml", "atom+xml")
     if accept.lower() not in accepted:
         raise ValueError('accept parameter must be one of ' +
                          ', '.join(accepted))
-    key = scopus.MY_API_KEY
+    try:
+        key = scopus.MY_API_KEY
+    except AttributeError:
+        load_api_key()
+        key = scopus.MY_API_KEY
     header = {'Accept': 'application/{}'.format(accept), 'X-ELS-APIKey': key}
     resp = requests.get(url, headers=header, params=params)
-    resp.raise_for_status()  # Raise status code if necessary
+    resp.raise_for_status()
     return resp
 
 
@@ -72,3 +81,23 @@ def get_content(qfile, refresh, *args, **kwds):
         with open(qfile, 'wb') as f:
             f.write(content)
     return content
+
+
+def load_api_key():
+    """Helper function to create file with API Key on the first run.
+    Function needs to be here because in __init__.py it causes problems
+    with RTFD.io.
+    """
+    SCOPUS_API_FILE = os.path.expanduser("~/.scopus/my_scopus.py")
+    with open(SCOPUS_API_FILE, "a+") as f:
+        f.seek(0)
+        exec(f.read(), globals())
+        if "MY_API_KEY" not in globals():
+            prompt = ("No API Key detected. Please enter a valid API Key "
+                      "obtained from http://dev.elsevier.com/myapikey.html: \n")
+            if sys.version_info >= (3, 0):
+                key = input(prompt)
+            else:
+                key = raw_input(prompt)
+            f.write('MY_API_KEY = "{}"'.format(key))
+            scopus.MY_API_KEY = key
