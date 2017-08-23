22b44fb42ac3f72cc74f1a87400a0e4dacfb7b40
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-08-23 16:12:30 -0400

    Take API Key loading out of __init__; fix #45 with python version detection

--- scopus/__init__.py
+++ scopus/__init__.py
@@ -11,17 +11,3 @@ from scopus.scopus_affiliation import *
 from scopus.scopus_author import *
 from scopus.scopus_reports import *
 from scopus.scopus_search import *
-
-
-SCOPUS_API_FILE = os.path.expanduser("~/.scopus/my_scopus.py")
-with open(SCOPUS_API_FILE, "a+") as f:
-    f.seek(0)
-    exec(f.read(), globals())
-    if "MY_API_KEY" not in globals():
-        prompt = ("No API key deteced. Please enter a valid API Key "
-                  "obtained from http://dev.elsevier.com/myapikey.html: \n")
-        if version_info >= (3, 0):
-            key = input(prompt)
-        else:
-            key = raw_input(prompt)
-        f.write('MY_API_KEY = "{}"'.format(key))
