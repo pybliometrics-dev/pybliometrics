2da635706ee926accad1a0003ed2178e6e589873
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-12-12 14:14:54 -0500

    Tests for CitationOverview

--- scopus/tests/__init__.py
+++ scopus/tests/__init__.py
@@ -1,3 +1,4 @@
+from scopus.tests.test_CitationOverview import *
 from scopus.tests.test_ScopusAbstract import *
 from scopus.tests.test_ScopusAffiliation import *
 from scopus.tests.test_ScopusAuthor import *
