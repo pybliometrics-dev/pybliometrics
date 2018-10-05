7ef02ccf09fd9388deb46bf07e4ba4f65905b0ec
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2018-10-05 22:23:49 +0200

    Rename setup.py to avoid clashes with nose

--- scopus/utils/__init__.py
+++ scopus/utils/__init__.py
@@ -1,4 +1,4 @@
 from scopus.utils.create_config import *
-from scopus.utils.setup import *
+from scopus.utils.startup import *
 from scopus.utils.get_content import *
 from scopus.utils.get_encoded_text import *
