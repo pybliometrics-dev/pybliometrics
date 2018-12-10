ec8040ba411167f28128f53851f2a714346c8513
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2018-12-10 13:14:26 +0100

    Update tests and add new test to ScopusSearch; remove tests for deprecated classes

--- scopus/tests/test_ScopusSearch.py
+++ scopus/tests/test_ScopusSearch.py
@@ -8,19 +8,24 @@ from nose.tools import assert_equal
 import scopus
 
 
-s = scopus.ScopusSearch('AU-ID(24320488600)', refresh=True)
+order = 'eid doi pii title subtype creator authname authid afid coverDate '\
+        'coverDisplayDate publicationName issn source_id aggregationType '\
+        'volume issueIdentifier pageRange citedby_count openaccess'
+doc = namedtuple('Document', order)
+
+s_au = scopus.ScopusSearch('AU-ID(24320488600)', refresh=False)
+s_j = scopus.ScopusSearch('SOURCE-ID(22900) AND PUBYEAR IS 2010', refresh=False)
 
 
-def test_get_eids():
-    assert_equal(s.get_eids(), ['2-s2.0-26444452434'])
+def test_get_eids_author():
+    assert_equal(s_au.get_eids(), ['2-s2.0-26444452434'])
 
 
-def test_results():
-    order = 'eid doi pii title subtype creator authname authid afid '\
-            'coverDate coverDisplayDate publicationName issn source_id '\
-            'aggregationType volume issueIdentifier pageRange citedby_count '\
-            'openaccess'
-    doc = namedtuple('Document', order)
+def test_get_eids_journal():
+    assert_equal(len(s_j.get_eids()), 125)
+
+
+def test_results_author():
     expected = doc(eid='2-s2.0-26444452434', doi='10.1016/0014-2921(92)90085-B',
         pii='001429219290085B', title='Economists as policymakers: A round-table discussion. Introduction',
         subtype='ar', creator='Draghi M.', authname='Draghi M.',
@@ -29,4 +34,16 @@ def test_results():
         issn='00142921', source_id='20749', aggregationType='Journal',
         volume='36', issueIdentifier='2-3', pageRange='307-309',
         citedby_count='1', openaccess='0')
-    assert_equal(s.results[-1], expected)
+    assert_equal(s_au.results[-1], expected)
+
+
+def test_results_journal():
+    expected = doc(eid='2-s2.0-77955427791', doi='10.1016/j.respol.2010.05.011',
+        pii='S0048733310001356', title='Comment on mowery, nelson and martin',
+        subtype='no', creator='Perrow C.', authname='Perrow C.',
+        authid='6602918903', afid='60005455', coverDate='2010-10-01',
+        coverDisplayDate='October 2010', publicationName='Research Policy',
+        issn='00487333', source_id='22900', aggregationType='Journal',
+        volume='39', issueIdentifier='8', pageRange='1030-1031',
+        citedby_count='6', openaccess='0')
+    assert_equal(s_j.results[-1], expected)
