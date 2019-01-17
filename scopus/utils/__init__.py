fc1bfb54c0658b94a1900e9ca685967b87b4bc0e
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2019-01-17 21:27:45 +0100

    Move function detect_id_type to get_content module

--- scopus/utils/__init__.py
+++ scopus/utils/__init__.py
@@ -1,5 +1,4 @@
 from scopus.utils.create_config import *
-from scopus.utils.detect_id_type import *
 from scopus.utils.get_content import *
 from scopus.utils.get_encoded_text import *
 from scopus.utils.parse_content import *
--- scopus/utils/get_content.py
+++ scopus/utils/get_content.py
@@ -9,6 +9,44 @@ errors = {400: exception.Scopus400Error, 401: exception.Scopus401Error,
           500: exception.Scopus500Error}
 
 
+def detect_id_type(sid):
+    """Method that tries to infer the type of abstract ID.
+
+    Parameters
+    ----------
+    sid : str
+        The ID of an abstract on Scopus.
+
+    Raises
+    ------
+    ValueError
+        If the ID type cannot be inferred.
+
+    Notes
+    -----
+    PII usually has 17 chars, but in Scopus there are valid cases with only
+    16 for old converted articles.
+
+    Scopus ID contains only digits, but it can have leading zeros.  If ID
+    with leading zeros is treated as a number, SyntaxError can occur, or the
+    ID will be rendered invalid and the type will be misinterpreted.
+    """
+    sid = str(sid)
+
+    if sid.startswith('2-s2.0-'):
+        return 'eid'
+    elif '/' in sid:
+        return 'doi'
+    elif 16 <= len(sid) <= 17:
+        return 'pii'
+    elif 10 <= len(sid) < 16 and sid.isnumeric():
+        return 'scopus_id'
+    elif len(sid) < 10 and sid.isnumeric():
+        return 'pubmed_id'
+    else:
+        raise ValueError('ID type detection failed for \'{}\'.'.format(sid))
+
+
 def download(url, params=None, accept="xml"):
     """Helper function to download a file and return its content.
 
