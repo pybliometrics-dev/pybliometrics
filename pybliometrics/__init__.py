b0b0a487fef01a77d609cde798525e1cf507fc85
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2019-06-17 16:31:37 +0200

    Fix semantic version string

--- pybliometrics/__init__.py
+++ pybliometrics/__init__.py
@@ -1,6 +1,6 @@
 from pbr.version import VersionInfo
 
-_v = VersionInfo('scopus').semantic_version()
+_v = VersionInfo('pybliometrics').semantic_version()
 __version__ = _v.release_string()
 version_info = _v.version_tuple()
 
