9ead8e0ab0bc9aaf81974c7b4b64875b3c7b32b2
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2020-04-02 13:27:52 +0200

    Use try-except instead of if-else

--- pybliometrics/scopus/utils/startup.py
+++ pybliometrics/scopus/utils/startup.py
@@ -1,19 +1,15 @@
-import os
-import warnings
-try:
-    import configparser
-except ImportError:
-    import ConfigParser as configparser
+import configparser
+from os.path import expanduser
 
-# Configuration setup
-config = configparser.ConfigParser()
-config.optionxform = str
-CONFIG_FILE = os.path.expanduser("~/.scopus/config.ini")
-if not os.path.exists(CONFIG_FILE):
+CONFIG_FILE = expanduser("~/.scopus/config.ini")
+try:
+    config = configparser.ConfigParser()
+    config.optionxform = str
+    config.read(CONFIG_FILE)
+except FileNotFoundError:
+    import warnings
     text = "pybliometrics did not find a configuration file.  Please issue "\
            "pybliometrics.scopus.utils.create_config() to start the process "\
            "which guides you through the generation of the configuration "\
            "file or read https://pybliometrics.readthedocs.io/en/stable/configuration.html."
     warnings.warn(text, UserWarning)
-else:
-    config.read(CONFIG_FILE)
