a8978b3e865166300b9a84d9784c88e61b21b918
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2022-01-01 16:03:52 +0100

    Attempt to cope with RTFD.io

--- pybliometrics/scopus/utils/startup.py
+++ pybliometrics/scopus/utils/startup.py
@@ -5,18 +5,16 @@ from pathlib import Path
 from pybliometrics.scopus.utils.constants import CONFIG_FILE, RATELIMITS
 from pybliometrics.scopus.utils.create_config import create_config
 
-# Read/create config file
-if not CONFIG_FILE.exists():
-    config = create_config()
-else:
-    config = configparser.ConfigParser()
-    config.optionxform = str
-    config.read(CONFIG_FILE)
-
-# Parse keys with fixture for RTFD.io
+# Read/create config file (with fixture for RTFD.io)
+config = configparser.ConfigParser()
+config.optionxform = str
 try:
+    if not CONFIG_FILE.exists():
+        config = create_config()
+    else:
+        config.read(CONFIG_FILE)
     KEYS = [k.strip() for k in config.get('Authentication', 'APIKey').split(",")]
-except configparser.NoSectionError:
+except EOFError:
     pass
 
 # Throttling params
