8889d263af4ccd6f8b6803316db188cec9a7a827
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2024-08-29 21:06:41 +0200

    Drop support for 3.6 and 3.7, add support for 3.12

--- pybliometrics/__init__.py
+++ pybliometrics/__init__.py
@@ -1,9 +1,4 @@
-import sys
-
-if sys.version_info >= (3, 8):
-    from importlib.metadata import version
-else:
-    from importlib_metadata import version
+from importlib.metadata import version
 
 __version__ = version("pybliometrics")
 
