3f93a6a107d95bd1678bd91812ff33416414a049
Nils Herrmann <nils18@live.com.mx>
2025-03-17 11:41:17 +0100

    Implementation of the ScDir Serial Title API

--- pybliometrics/sciencedirect/__init__.py
+++ pybliometrics/sciencedirect/__init__.py
@@ -6,4 +6,5 @@ from pybliometrics.sciencedirect.article_retrieval import *
 from pybliometrics.sciencedirect.object_metadata import *
 from pybliometrics.sciencedirect.object_retrieval import *
 from pybliometrics.sciencedirect.sciencedirect_search import *
+from pybliometrics.sciencedirect.serial_title import *
 from pybliometrics.sciencedirect.subject_classifications import *
