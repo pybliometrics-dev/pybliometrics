a26814a35b23e23472abc243fc1802b015f5e805
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2025-06-06 09:32:11 +0200

    Simplify default cache path generation

--- pybliometrics/utils/constants.py
+++ pybliometrics/utils/constants.py
@@ -1,44 +1,43 @@
 from pathlib import Path
 
 # Location of configuration file with legacy support
-config_options = [
+config_path_options = [
     Path.home() / ".scopus" / "config.ini",
     Path.home() / ".pybliometrics" / "Scopus" / "config.ini",
     Path.home() / ".config" / "pybliometrics.cfg"  # default
 ]
-CONFIG_FILE = next((path for path in config_options if path.exists()),
-                   config_options[-1])
+CONFIG_FILE = next((path for path in config_path_options if path.exists()),
+                   config_path_options[-1])
 
 # Location of cache with legacy support
-base_path_options = [
+cache_path_options = [
     Path.home() / ".scopus",
-    Path.home() / ".pybliometrics" / "Scopus",
-    Path.home() / ".cache" / "pybliometrics" / "Scopus"  # default
+    Path.home() / ".pybliometrics",
+    Path.home() / ".cache" / "pybliometrics"  # default
 ]
-BASE_PATH_SCOPUS = next((path for path in base_path_options if path.exists()),
-                        base_path_options[-1])
-BASE_PATH_SCIENCEDIRECT = BASE_PATH_SCOPUS.parent / "ScienceDirect"
+CACHE_PATH = next((path for path in cache_path_options if path.exists()),
+                 cache_path_options[-1])
 
 DEFAULT_PATHS = {
-    'AbstractRetrieval': BASE_PATH_SCOPUS / 'abstract_retrieval',
-    'AffiliationRetrieval': BASE_PATH_SCOPUS / 'affiliation_retrieval',
-    'AffiliationSearch': BASE_PATH_SCOPUS / 'affiliation_search',
-    'AuthorRetrieval': BASE_PATH_SCOPUS / 'author_retrieval',
-    'AuthorSearch': BASE_PATH_SCOPUS / 'author_search',
-    'ArticleEntitlement': BASE_PATH_SCIENCEDIRECT / 'article_entitlement',
-    'ArticleMetadata': BASE_PATH_SCIENCEDIRECT / 'article_metadata',
-    'ArticleRetrieval': BASE_PATH_SCIENCEDIRECT / 'article_retrieval',
-    'NonserialTitle': BASE_PATH_SCIENCEDIRECT / 'nonserial_title',
-    'CitationOverview': BASE_PATH_SCOPUS / 'citation_overview',
-    'ObjectMetadata': BASE_PATH_SCIENCEDIRECT / 'object_metadata',
-    'ObjectRetrieval': BASE_PATH_SCIENCEDIRECT / 'object_retrieval',
-    'PlumXMetrics': BASE_PATH_SCOPUS / 'plumx',
-    'ScDirSubjectClassifications': BASE_PATH_SCIENCEDIRECT / 'subject_classification',
-    'ScienceDirectSearch': BASE_PATH_SCIENCEDIRECT / 'science_direct_search',
-    'ScopusSearch': BASE_PATH_SCOPUS / 'scopus_search',
-    'SerialSearch': BASE_PATH_SCOPUS / 'serial_search',
-    'SerialTitle': BASE_PATH_SCOPUS / 'serial_title',
-    'SubjectClassifications': BASE_PATH_SCOPUS / 'subject_classification',
+    'AbstractRetrieval': CACHE_PATH / "Scopus" / 'abstract_retrieval',
+    'AffiliationRetrieval': CACHE_PATH / "Scopus" / 'affiliation_retrieval',
+    'AffiliationSearch': CACHE_PATH / "Scopus" / 'affiliation_search',
+    'AuthorRetrieval': CACHE_PATH / "Scopus" / 'author_retrieval',
+    'AuthorSearch': CACHE_PATH / "Scopus" / 'author_search',
+    'ArticleEntitlement': CACHE_PATH / "ScienceDirect" / 'article_entitlement',
+    'ArticleMetadata': CACHE_PATH / "ScienceDirect" / 'article_metadata',
+    'ArticleRetrieval': CACHE_PATH / "ScienceDirect" / 'article_retrieval',
+    'NonserialTitle': CACHE_PATH / "ScienceDirect" / 'nonserial_title',
+    'CitationOverview': CACHE_PATH / "Scopus" / 'citation_overview',
+    'ObjectMetadata': CACHE_PATH / "ScienceDirect" / 'object_metadata',
+    'ObjectRetrieval': CACHE_PATH / "ScienceDirect" / 'object_retrieval',
+    'PlumXMetrics': CACHE_PATH / "Scopus" / 'plumx',
+    'ScDirSubjectClassifications': CACHE_PATH / "ScienceDirect" / 'subject_classification',
+    'ScienceDirectSearch': CACHE_PATH / "ScienceDirect" / 'science_direct_search',
+    'ScopusSearch': CACHE_PATH / "Scopus" / 'scopus_search',
+    'SerialSearch': CACHE_PATH / "Scopus" / 'serial_search',
+    'SerialTitle': CACHE_PATH / "Scopus" / 'serial_title',
+    'SubjectClassifications': CACHE_PATH / "Scopus" / 'subject_classification',
 }
 
 # URLs for all classes
