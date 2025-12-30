1f4175edc2c534f7654a80ce2887a927e79d4cdd
Nils A. Herrmann <88451442+nils-herrmann@users.noreply.github.com>
2025-12-30 07:08:17 +0100

    Handle None in Organization (close #425)

--- pybliometrics/scopus/abstract_retrieval.py
+++ pybliometrics/scopus/abstract_retrieval.py
@@ -349,7 +349,7 @@ class AbstractRetrieval(Retrieval):
                 try:
                     org = org['$']
                 except TypeError:  # Multiple names given
-                    org = "; ".join([d['$'] for d in org])
+                    org = "; ".join([d['$'] for d in org if d])
             except KeyError:
                 org = None
             new = Correspondence(surname=item.get('person', {}).get('ce:surname'),
